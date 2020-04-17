405e745e678642b5b8dca9cb472920920a2dd532
Andy Friedman <38634955+andyfriedman@users.noreply.github.com>
2020-04-17 13:16:36 -0700

    Add StringReverse project

---
+++ StringReverse/StringReverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringReverse
+{
+    /// <summary>
+    /// Write a function to reverse a string without allocating any new strings in memory
+    /// </summary>
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            Console.Write("Enter a string to be reversed: ");
+            var str = Console.ReadLine();
+
+            var reversed = Reverse(str);
+
+            Console.WriteLine(reversed);
+        }
+
+        unsafe static string Reverse(string str)
+        {
+            fixed (char* pStr = str)
+            {
+                int start = 0, end = str.Length - 1;
+                while (start < end)
+                {
+                    pStr[start] ^= pStr[end];
+                    pStr[end] ^= pStr[start];
+                    pStr[start] ^= pStr[end];
+                    ++start; --end;
+                }
+                return str;
+            }
+        }
+    }
+}
